16221a472dae79a97667a881877023f8252eb506
Carlos Augusto Restrepo <pg05carlos@vfs.com>
2017-09-28 12:01:54 -0700

    Color + Elevation
    Added color blending between cells
    Added elevation

--- GridProject/Assets/Scripts/MapGeneration/HexCell.cs
+++ GridProject/Assets/Scripts/MapGeneration/HexCell.cs
@@ -6,4 +6,42 @@ public class HexCell : MonoBehaviour
     public HexCoordinates coordinates;
 
     public Color color;
+
+    int elevation;
+
+    public RectTransform uiRect;
+
+    public int Elevation
+    {
+        get
+        {
+            return elevation;
+        }
+        set
+        {
+            elevation = value;
+
+            Vector3 position = transform.localPosition;
+            position.y = value * HexMetrics.elevationStep;
+            transform.localPosition = position;
+
+            Vector3 uiPosition = uiRect.localPosition;
+            uiPosition.z = elevation * -HexMetrics.elevationStep;
+            uiRect.localPosition = uiPosition;
+        }
+    }
+
+    [SerializeField]
+    HexCell[] neighbors = new HexCell[6];
+
+    public HexCell GetNeighbor(HexDirection direction)
+    {
+        return neighbors[(int)direction];
+    }
+
+    public void SetNeighbor(HexDirection direction, HexCell cell)
+    {
+        neighbors[(int)direction] = cell;
+        cell.neighbors[(int)direction.Opposite()] = this;
+    }
 }
--- GridProject/Assets/Scripts/MapGeneration/HexMapEditor.cs
+++ GridProject/Assets/Scripts/MapGeneration/HexMapEditor.cs
@@ -8,6 +8,8 @@ public class HexMapEditor : MonoBehaviour
     public HexGrid hexGrid;
 
     private Color activeColor;
+    
+    private int activeElevation;
 
     void Awake()
     {
@@ -22,14 +24,26 @@ public class HexMapEditor : MonoBehaviour
         }
     }
 
+    public void SetElevation(float elevation)
+    {
+        activeElevation = (int)elevation;
+    }
+
     void HandleInput()
     {
         Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         if (Physics.Raycast(inputRay, out hit))
         {
-            hexGrid.ColorCell(hit.point, activeColor);
+            EditCell(hexGrid.GetCell(hit.point));
         }
+    }
+
+    void EditCell(HexCell cell)
+    {
+        cell.color = activeColor;
+        cell.Elevation = activeElevation;
+        hexGrid.Refresh();
     }
 
     public void SelectColor(int index)
